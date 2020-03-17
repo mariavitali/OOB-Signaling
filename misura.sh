d1dfa73ac1db8a123aec4ff5a9b9fe209a7fec20
mariavitali <m.vitali7@studenti.unipi.it>
2020-03-17 12:40:50 +0100

    read and calc fixed

--- misura.sh
+++ misura.sh
@@ -3,9 +3,11 @@
 #input: nomi di file contenenti gli output di supervisor ($1) e client ($2)
 
 nClients=0
+nEstimates=0
 correctEst=0
 errSum=0
 avgErr=0
+
 
 #controllo che i parametri di input siano file ed esistano
 if ! [[ -f $1 ]]; then
@@ -19,43 +21,42 @@ if ! [[ -f $2 ]]; then
 fi
 
 #apro i file in lettura
-exec 3<$1
-exec 4<$2
+#exec 3<$1
+#exec 4<$2
 
 echo "Inizio la misura..."
 
+declare -a arrayid
+declare -A secrets
+declare -A estimates
+
 #leggo il file del client e ricavo gli id e i relativi secrets
-done=DONE
-i=0
-while read -u 4 line; do
-    if [[ "$line" == *"$done"* ]]; then continue
-    else 
-        arrayid[$i]=${line:7:16}
-        secrets[$i]=${line:31}
-        ((i++))
+
+while IFS= read -r line; do
+    ROW=($line)
+    if [[ "$line" == *"SECRET"* ]]; then
+        #avevo fatto diversamente ma confrontandomi con i colleghi, mi hanno fatto notare che gli array possono essere sparsi
+        #usare questa caratteristica degli array bash mi è sembrato molto conveniente e intelligente
+        id=${ROW[1]}
+        secrets[$id]=${ROW[3]}
         ((nClients++))
     fi
-done
+done <$2
 
 #leggo il file del supervisor e creo un array con le stime dei secrets associati agli id dei clients in arrayid
-i=0
-while read -u 3 line; do
-    LINEA=($line)
-    j=0
-    found=0
-    while [[ $j<${nClients} && ${found}==0 ]]; do
-        if [[ "$line" == *"${arrayid[$j]}"* ]]; then
-            found=1
-            estimates[$i]=${LINEA[2]}
-        else ((j++))
-        fi
-    done
-    ((i++))
-done
+while IFS= read -r line; do
+    ROW=($line)
+    if [[ "$line" == *"BASED"* ]]; then
+        id=${ROW[4]}
+        arrayIdEst[$nEstimates]=$id
+        estimates[$id]=${ROW[2]}
+        ((nEstimates++))
+    fi  
+done <$1
 
 #inizio le stime
-for ((i=0; i<${nClients}; i++)); do
-    diff=$((estimates[$i]-secrets[$i]))
+for id in "${arrayIdEst[@]}"; do
+    diff=$((${estimates[$id]} - ${secrets[$id]}))
     if [[ diff -lt 0 ]]; then
         diff=$((-${diff}))
     fi
@@ -67,8 +68,9 @@ done
 
 avgErr=$((${errSum} / ${nClients}))
 
-echo "Total clients and estimates: ${nClients}"
-echo "Supervisor correct estimates: ${correctEst}/${nClients}"
-echo "Supervisor wrong estimates: $((${nClients} - ${correctEst}))/${nClients}"
+echo "Total clients run: ${nClients}"
+echo "Total estimates: ${nEstimates}"
+echo "Supervisor correct estimates: ${correctEst}/${nEstimates}"
+echo "Supervisor wrong estimates: $((${nClients} - ${correctEst}))/${nEstimates}"
 echo "Average error: ${avgErr}"
 echo "DONE!"
